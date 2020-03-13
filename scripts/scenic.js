b9b60ab9b86f0450c375b38026ca00d50332d023
RichardrZh <34480193+RichardrZh@users.noreply.github.com>
2020-03-13 03:46:47 -0400

    Update scenic.js

--- scripts/scenic.js
+++ scripts/scenic.js
@@ -1,11 +1,25 @@
 window.onload = function(){
+
+    // skew and spacing
     let skew = document.getElementById('skew');
-    let current = -10;
+    let spacing = document.getElementById('spacing');
+    let dim = document.getElementById('header_content');
+
     window.addEventListener('scroll', function(){
-        let value = Math.min(-10 + window.scrollY/60, 0);
-        console.log(value);
-        skew.style.transform = "skewY(" + value + "deg)";
+        // vw = px * ratio, px = vw / ratio
+        let ratio = 100 / Math.max(document.documentElement.clientWidth, 1);
+        let scroll = window.scrollY;
+
+        // skew
+        let value_skew = Math.min(-10 + scroll/60, 0);
+
+        // spacing
+        // in vw
+        let dimY = dim.getBoundingClientRect().bottom;
+        let value_spacing = Math.min((scroll - dimY) / 10 * ratio, 6);
+
+        spacing.style.height = value_spacing + "vw";
+        skew.style.transform = "skewY(" + value_skew + "deg)";
     });
+
 };
-
-
